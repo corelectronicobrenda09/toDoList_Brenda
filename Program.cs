9d28e503101aa900aef93fd7cbb0f6a34e68c23a
corelectronicobrenda09 <brendavargas621@gmail.com>
2026-01-28 13:24:17 -0400

    Add files via upload
    Corrección del programa, disculpe profesor pero corregí lo que usted había especificado. 
    pero gracias a Dios poco a poco voy comprendiendo.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,13 @@
-﻿// Variables y principales tipos de datos
-string nombre = "Brenda";
-string apellido = "Vargas";
-char inicialNombre = 'B';
-/*
-  En C#, los números enteros son un tipo de valor que representa números sin decimales. Los números enteros en C# incluyen los siguientes tipos: byte, short, int, long.
-*/
-int edad = 40;
-bool esProfesor = true;
-
-/*
-  Los números flotantes en C# incluyen los siguientes tipos: float y double.
-*/
-float altura = 6.7f;
-
-Console.WriteLine($"Hola, mi nombre es {nombre} {apellido}");
-Console.WriteLine("Tengo " + edad + " años de edad.\n");
-
-
-// Entrada de datos
-Console.Write("Nombre: ");
-string? nombreUsuario = Console.ReadLine(); // El símbolo ? indica que puede ser string o null
+﻿Console.Write("Nombre: ");
+string nombre = Console.ReadLine();
 
 Console.Write("Apellido: ");
-string? apellidoUsuario = Console.ReadLine();
+string apellido = Console.ReadLine();
 
-Console.WriteLine($"¡Bienvenido {nombreUsuario} {apellidoUsuario}!");
+Console.Write("Edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.Write("Altura: ");
+float altura = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"\nHola, mi nombre es {nombre} {apellido}, tengo {edad} años y mido {altura}.");
